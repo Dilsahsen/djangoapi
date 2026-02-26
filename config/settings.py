fd0308984302330d578364362c05547c1695fc4b
Dilşah Şen <dillisah11@gmail.com>
2026-02-27 01:37:52 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@#w+vbqtnxhyvj-e*w0u729gmif_1q50vo7r%a2b1omi7_m_*k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dilsahsen.pythonanywhere.com"]
 
 
 # Application definition
@@ -200,6 +200,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticFiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
